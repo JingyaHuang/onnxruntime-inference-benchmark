3fc3cc715bebc412c311759daea805a14f78f6da
JingyaHuang <huang_jingya@outlook.com>
2022-11-28 15:33:10 +0000

    Improve legend clarity

--- benchmarks/onnxruntime/draw.py
+++ benchmarks/onnxruntime/draw.py
@@ -3,35 +3,33 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Load dataframe
-path = 't4_res_ort_gpt2_greedy.pkl'
+path = 't4_res_ort_t5_s_greedy.pkl'
 df_greedy = pd.read_pickle(path)
-path = 't4_res_ort_gpt2_beam5.pkl'
+path = 't4_res_ort_t5_s_beam5.pkl'
 df_beam = pd.read_pickle(path)
 df = pd.concat([df_greedy, df_beam], ignore_index=True)
 df["seq_len_str"] = df['seq_len'].astype(str)
-# df["Framework"] = df.apply(lambda row: "PyTorch" if row.framework=="pt" else "ORT", axis=1)
 df["Framework"] = df.apply(lambda row: "PyTorch" if row.framework=="PyTorch" else "ORT", axis=1)
 df["Generation"] = df.apply(lambda row: "Beam(5)" if row.num_beam>1 else "Greedy", axis=1)
 
 # Draw
 
-# hue = "(" + df['Framework'].astype(str) + ', ' + df['Generation'].astype(str) + ")"
 sns.set_style("darkgrid", {"grid.color": ".6", "grid.linestyle": ":"})
 ax_s = sns.scatterplot(
     x='seq_len_str', y='time_ms_per_token',
-    data=df, hue="Framework", style="Generation"
+    data=df, hue="Framework", style="Generation", legend='full'
 )
-ax_s.get_legend().remove()
 ax = sns.lineplot(
     x='seq_len_str', y='time_p95_ms_per_token',
     data=df, hue="Framework", style="Generation", legend="full"
-) #, orient="v", palette="deep", dodge=True)
+)
 sns.move_legend(ax, "upper left", bbox_to_anchor=(1, 1))
+handles, labels = ax.get_legend_handles_labels()
+plt.legend(handles[6:], labels[6:], bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
 plt.title(
-    "GPT2 inference: PyTorch V.S. ONNX Runtime"
+    "T5-small inference: PyTorch V.S. ONNX Runtime"
 )
 plt.xlabel("Sequence Length")
-# plt.ylim(0, 10)
 plt.ylabel("Per Token Latency (ms)")
 plt.tight_layout()
-plt.savefig("t4_res_ort_gpt2.png", dpi=900)
+plt.savefig("t4_res_ort_t5_s.png", dpi=900)
